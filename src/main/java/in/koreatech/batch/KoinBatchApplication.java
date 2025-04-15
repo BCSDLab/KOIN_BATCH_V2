0fe25ecfd08ad5643c565d0eda162e6072f9789c
신관규 <soundbar91@gmail.com>
2025-04-15 14:45:27 +0900

    feat: 데이터 데이터 테이블 설정 클래스 추가

--- src/main/java/in/koreatech/batch/KoinBatchApplication.java
+++ src/main/java/in/koreatech/batch/KoinBatchApplication.java
@@ -2,8 +2,10 @@ package in.koreatech.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class KoinBatchApplication {
 
 	public static void main(String[] args) {
