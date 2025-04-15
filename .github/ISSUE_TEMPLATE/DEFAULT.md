918f850a7265b2e5dd018d9ed631009aacd76fe9
신관규 <soundbar91@gmail.com>
2025-04-15 10:40:21 +0900

    feat: 프로젝트 초기 세팅 (#1)
    * feat: 프로젝트 초기 세팅
    
    * feat: 이슈 템플릿 추가
    
    * feat: PR 템플릿 추가

---
+++ .github/ISSUE_TEMPLATE/DEFAULT.md
@@ -0,0 +1,11 @@
+---
+name: 이슈
+about: 이슈에 대한 설명을 적어주세요.
+title: ''
+labels: ''
+assignees: ''
+
+---
+
+## 🔴 이슈 설명
+
---
+++ src/test/java/in/koreatech/batch/KoinBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package in.koreatech.batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoinBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
