773f7a2a9fc09f0c015bf07f8b737817c5fd89bf
신관규 <soundbar91@gmail.com>
2025-04-16 21:11:15 +0900

    feat: 식단 메뉴 리파지토리 추가

---
+++ src/main/java/in/koreatech/batch/domain/dining/repository/DiningMenuRepository.java
@@ -0,0 +1,8 @@
+package in.koreatech.batch.domain.dining.repository;
+
+import org.springframework.data.repository.Repository;
+
+import in.koreatech.batch.domain.dining.model.DiningMenu;
+
+public interface DiningMenuRepository extends Repository<DiningMenu, Integer> {
+}
