4ee300d2cb4a8cb44b5a19bd08b85eb6ee4b9a0f
신관규 <soundbar91@gmail.com>
2025-04-17 16:56:59 +0900

    fix: 로그인 쿠키 레디스 리파지토리 클래스 수정

--- src/main/java/in/koreatech/batch/_common/redis/LoginCookieRedisRepository.java
+++ src/main/java/in/koreatech/batch/_common/redis/LoginCookieRedisRepository.java
@@ -11,20 +11,15 @@ import java.util.Optional;
 @RequiredArgsConstructor
 public class LoginCookieRedisRepository {
 
-    private final StringRedisTemplate redisTemplate;
-
     private static final String COOKIE_KEY = "__KSMSID__";
-    private static final Duration COOKIE_TTL = Duration.ofHours(1);
+
+    private final StringRedisTemplate redisTemplate;
 
     public Optional<String> getLoginCookie() {
         return Optional.ofNullable(redisTemplate.opsForValue().get(COOKIE_KEY));
     }
 
     public void setLoginCookie(String cookieValue) {
-        redisTemplate.opsForValue().set(COOKIE_KEY, cookieValue, COOKIE_TTL);
-    }
-
-    public void clearLoginCookie() {
-        redisTemplate.delete(COOKIE_KEY);
+        redisTemplate.opsForValue().set(COOKIE_KEY, cookieValue);
     }
 }
