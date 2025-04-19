b44ee66536d8776e7cd36bee8d59b6f4e92ff5de
신관규 <soundbar91@gmail.com>
2025-04-19 14:42:57 +0900

    refactor: 설정파일 주입

--- src/main/java/in/koreatech/batch/_common/redis/LoginCookieRedisRepository.java
+++ src/main/java/in/koreatech/batch/_common/redis/LoginCookieRedisRepository.java
@@ -5,21 +5,21 @@ import java.util.Optional;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import in.koreatech.batch._common.config.PortalProperties;
 import lombok.RequiredArgsConstructor;
 
 @Component
 @RequiredArgsConstructor
 public class LoginCookieRedisRepository {
 
-    private static final String COOKIE_KEY = "__KSMSID__";
-
     private final StringRedisTemplate redisTemplate;
+    private final PortalProperties portalProperties;
 
     public Optional<String> getLoginCookie() {
-        return Optional.ofNullable(redisTemplate.opsForValue().get(COOKIE_KEY));
+        return Optional.ofNullable(redisTemplate.opsForValue().get(portalProperties.cookieLogin()));
     }
 
     public void setLoginCookie(String cookieValue) {
-        redisTemplate.opsForValue().set(COOKIE_KEY, cookieValue);
+        redisTemplate.opsForValue().set(portalProperties.cookieLogin(), cookieValue);
     }
 }
