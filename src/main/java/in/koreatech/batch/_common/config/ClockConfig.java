8de70df6301fc0031bdbf1da3ec83f275b5cc8c3
신관규 <soundbar91@gmail.com>
2025-04-21 15:42:10 +0900

    feat: Clock 설정 클래스 추가

---
+++ src/main/java/in/koreatech/batch/_common/config/ClockConfig.java
@@ -0,0 +1,20 @@
+package in.koreatech.batch._common.config;
+
+import java.time.Clock;
+import java.time.ZoneId;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("!test")
+public class ClockConfig {
+
+    private final ZoneId zoneId = ZoneId.of("Asia/Seoul");
+
+    @Bean
+    public Clock clock() {
+        return Clock.system(zoneId);
+    }
+}
