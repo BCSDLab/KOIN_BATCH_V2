b2564a8b7ae0346cc92e103cba41b48e3fdec736
신관규 <soundbar91@gmail.com>
2025-04-25 01:18:58 +0900

    feat: 버스 노선 정보 요청 로직 구현

--- src/main/java/in/koreatech/batch/domain/bus/client/CityBusClient.java
+++ src/main/java/in/koreatech/batch/domain/bus/client/CityBusClient.java
@@ -1,6 +1,7 @@
 package in.koreatech.batch.domain.bus.client;
 
 import static in.koreatech.batch.domain.bus.dto.CityBusRouteIdApiResponse.InnerRoute;
+import static in.koreatech.batch.domain.bus.dto.CityBusRouteInfoApiResponse.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import in.koreatech.batch.domain.bus.dto.CityBusRouteIdApiResponse;
+import in.koreatech.batch.domain.bus.dto.CityBusRouteInfoApiResponse;
 import in.koreatech.batch.domain.bus.exception.CityBusRouteApiException;
 import in.koreatech.batch.integration.config.BusProperties;
 import lombok.RequiredArgsConstructor;
@@ -49,5 +51,23 @@ public class CityBusClient {
         }
 
         return routeIds;
+    }
+
+    public List<CityBusRouteInfo> requestCityBusRouteInfos(Integer cityBusRouteId) {
+        Request request = new Request.Builder()
+            .url(String.format(busProperties.url().routeInfo(), cityBusRouteId))
+            .build();
+        List<CityBusRouteInfo> routeInfos = new ArrayList<>();
+
+        try (Response response = okHttpClient.newCall(request).execute()) {
+            if (response.body() != null) {
+                String responseBody = response.body().string();
+                routeInfos.addAll(objectMapper.readValue(responseBody, CityBusRouteInfoApiResponse.class).resultList());
+            }
+        } catch (IOException e) {
+            throw CityBusRouteApiException.withDetail("버스 노선 정보 요청 중 오류가 발생했습니다.");
+        }
+
+        return routeInfos;
     }
 }
