3e3bdb2baf96b7d689e6586d6e3a89d1c57a80e2
신관규 <soundbar91@gmail.com>
2025-04-25 02:06:31 +0900

    feat: writer 클래스 추가

---
+++ src/main/java/in/koreatech/batch/domain/bus/writer/CityBusTimetableWriter.java
@@ -0,0 +1,23 @@
+package in.koreatech.batch.domain.bus.writer;
+
+import java.util.List;
+
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+
+import in.koreatech.batch.domain.bus.model.CityBusTimetable;
+import in.koreatech.batch.domain.bus.repository.CityBusTimetableBatchRepository;
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class CityBusTimetableWriter implements ItemWriter<CityBusTimetable> {
+
+    private final CityBusTimetableBatchRepository cityBusTimetableBatchRepository;
+
+    @Override
+    public void write(Chunk<? extends CityBusTimetable> chunk) {
+        cityBusTimetableBatchRepository.saveAll((List<CityBusTimetable>)chunk);
+    }
+}
