57fa3a8aa2d2395cea8b8bf7d7a2615911d44964
신관규 <soundbar91@gmail.com>
2025-07-10 23:12:11 +0900

    feat: EnableScheduling 어노테이션 추가

--- src/main/java/in/koreatech/batch/KoinBatchApplication.java
+++ src/main/java/in/koreatech/batch/KoinBatchApplication.java
@@ -3,7 +3,9 @@ package in.koreatech.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 @ConfigurationPropertiesScan
 public class KoinBatchApplication {
