bbea4338209882773ced6cbd79d195dfd6fe1547
신관규 <soundbar91@gmail.com>
2025-07-10 22:21:12 +0900

    feat: 아침, 점심, 저녁 스케쥴러 추가

--- src/main/java/in/koreatech/batch/domain/dining/scheduler/MenuJobScheduler.java
+++ src/main/java/in/koreatech/batch/domain/dining/scheduler/MenuJobScheduler.java
@@ -11,8 +11,8 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-@RequiredArgsConstructor
 @Component
+@RequiredArgsConstructor
 public class MenuJobScheduler {
 
     private final JobLauncher jobLauncher;
@@ -30,6 +30,49 @@ public class MenuJobScheduler {
                 mealMenuPollingJob,
                 new JobParametersBuilder()
                     .addLong("timestamp", System.currentTimeMillis()) // 매 실행마다 파라미터 다르게
+                    .toJobParameters()
+            );
+        } catch (Exception e) {
+            log.error("식단 Job 실행 중 오류 발생", e);
+        }
+    }
+
+    @Scheduled(cron = "0 0 6,8 * * *")
+    public void updateBreakfastMenu() {
+        try {
+            jobLauncher.run(
+                mealMenuPollingJob,
+                new JobParametersBuilder()
+                    .addLong("timestamp", System.currentTimeMillis())
+                    .toJobParameters()
+            );
+        } catch (Exception e) {
+            log.error("식단 Job 실행 중 오류 발생", e);
+        }
+    }
+
+    @Scheduled(cron = "0 30 10,11 * * *")
+    public void updateLunchMenu() {
+        try {
+            jobLauncher.run(
+                mealMenuPollingJob,
+                new JobParametersBuilder()
+                    .addLong("timestamp", System.currentTimeMillis())
+                    .toJobParameters()
+            );
+        } catch (Exception e) {
+            log.error("식단 Job 실행 중 오류 발생", e);
+        }
+    }
+
+    @Scheduled(cron = "0 0 15 * * *")
+    @Scheduled(cron = "0 30 17 * * *")
+    public void updateDinnerMenu() {
+        try {
+            jobLauncher.run(
+                mealMenuPollingJob,
+                new JobParametersBuilder()
+                    .addLong("timestamp", System.currentTimeMillis())
                     .toJobParameters()
             );
         } catch (Exception e) {
