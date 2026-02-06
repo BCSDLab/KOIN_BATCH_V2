1e26fdf2cc6ad4ddb149cbbb9ccf9f10ab04208d
신관규 <soundbar91@gmail.com>
2026-02-06 21:50:59 +0900

    feat: 암호화 포털 계정 비밀번호 게터 메소드 추가

--- src/main/java/in/koreatech/batch/integration/config/PortalProperties.java
+++ src/main/java/in/koreatech/batch/integration/config/PortalProperties.java
@@ -1,5 +1,10 @@
 package in.koreatech.batch.integration.config;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "portal")
@@ -10,6 +15,20 @@ public record PortalProperties(
     String cookie,
     Url url
 ) {
+    public String securePw() {
+        String credsDir = System.getenv("CREDENTIALS_DIRECTORY");
+        if (credsDir == null) {
+            return this.pw;
+        }
+
+        try {
+            Path path = Paths.get(credsDir, "portal_password");
+            return Files.readString(path).trim();
+        } catch (IOException e) {
+            throw new RuntimeException("운영 환경에서 Portal 자격 증명을 로드하지 못했습니다.", e);
+        }
+    }
+
     public record Url(
         String home,
         String checkLoginId,
